482fd7c0a735c1564a3e831c51369adb7cd102c3
Mario Enrique Carbajal Lopez <Marencarlo2112@gmail.com>
2022-04-21 14:44:08 -0600

    Creacion del nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css';
-import Button from '@mui/material/Button';
-
+import NavBar from './components/navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#FFF',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+      text: {
+        secondary: 'rgba(0,0,0,0.87)',
+      },
+    },
+  });
   return (
     <div className="App">
-      <Button variant="contained">Hello World</Button>
+      <ThemeProvider theme={lightTheme}>
+        <NavBar />
+      </ThemeProvider>
     </div>
   );
 }
